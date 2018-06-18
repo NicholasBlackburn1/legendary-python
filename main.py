85cc595334b9af2ee41b3f32517eae1ea1a36e7c
NICKY B <Nickblackburn02@gmail.com>
2018-06-18 14:54:56 -0400

    added code fix

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+import time
+
+print 'welcome'
+print 'i will think'
+time.sleep(5)
+print 'i thought for', random.randint(1,200), 'seconds'
---
+++ python-loding-screen.py
@@ -0,0 +1,7 @@
+# imports libs
+import snaps
+import time
+#uses snaps.py for display
+snaps.display_message ('Python Game')
+time.sleep(3)
+snaps.display_message('nick was here', color=(255, 0, 0), size=50, horiz='right', vert='top')
