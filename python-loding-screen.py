829d48c0c819a4eef727fd2ac273b38dcedf4352
NICKY B <Nickblackburn02@gmail.com>
2018-06-18 15:05:02 -0400

    added logo

--- python-loding-screen.py
+++ python-loding-screen.py
@@ -4,4 +4,4 @@ import time
 #uses snaps.py for display
 snaps.display_message ('Python Game')
 time.sleep(3)
-snaps.display_message('nick was here', color=(255, 0, 0), size=50, horiz='right', vert='top')
+snaps.display_image()
