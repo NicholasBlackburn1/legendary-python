34fd077b20c93789ea25be7a9c53f2a182240a7c
NICKY B <Nickblackburn02@gmail.com>
2018-06-18 16:49:41 -0400

    added game

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 # imports
 import random
 import time
+import os
+import system
 # vars
 deaths = 0
 total = 0
 usersuccess = 0
 userfail = 0
 life = 0
+name = 0
 
 #var adds
 total = usersuccess + life
 deaths = userfail
 life = usersuccess - userfail
+#
+print 'Welcome to the Python Game'
+time.sleep(1)
+name = input ('What is your name?')
+ print name , ('Is this correct')
+ yes = input('type y for yes or n for no')
+ if yes =  click.getchar(y) os.system(python )
